b8a16bf2b64895925e02f8667c0d05fd44223b9a
Damian Bravi <damianbravi@gmail.com>
2025-07-20 13:03:23 +0300

    commenting test

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -2,6 +2,9 @@ import { test } from '../src/fixtures/mergeFixtures';
 import testData from '../src/data/testData.json';
 const { submitSuccess } = testData.validations.practiceForm;
 
+// Test uses global fixture to navigate to the Forms section and fill the Practice Form using named fixture
+// Console logs are added to track the flow of the test and fixture execution
+
 test.use({
     dashboardOptions: {
         section: 'Forms',
