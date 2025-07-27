b2dd2484476a3085894733ffeb170d2b3ed1460c
Damian Bravi <damianbravi@gmail.com>
2025-07-27 11:57:14 +0300

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     use: {
         headless: true,
         actionTimeout: 0,
-        trace: 'on-first-retry',
+        trace: 'on',
         baseURL: 'https://demoqa.com/',
     },
     reporter: [
